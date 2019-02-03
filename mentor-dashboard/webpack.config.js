ba04216172737ef764df1ffb909af4343be42392
gubnich <gubnich@yandex.ru>
2019-02-03 12:28:00 +0300

    build: add babel to  webpack

--- mentor-dashboard/webpack.config.js
+++ mentor-dashboard/webpack.config.js
@@ -5,5 +5,20 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
