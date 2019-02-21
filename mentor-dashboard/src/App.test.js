c8c11eac86b9dacd51304d665c3d13af21818ee0
gubnich <gubnich@yandex.ru>
2019-02-21 19:21:29 +0300

    test: add test for rendering

--- mentor-dashboard/src/App.test.js
+++ mentor-dashboard/src/App.test.js
@@ -1,9 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({adapter: new Adapter()});
+
+it('checks the components rendering', () => {
+  const mock = {
+    "tasks": {
+     "names": ["testTaskName"],
+     "links": ["testTaskLink"],
+     "statuses": ["testTaskStatus"]
+    },
+    "mentors": {
+     "testMentor": {
+      "name": "testMentorName",
+      "login": "testMentor",
+      "city": "testCity",
+      "students": {
+       "testStudent": {
+        "score": [
+         {
+          "linkPR": "testLink",
+          "mark": "testMark"
+         },
+        
+        ],
+        "gitHubLink": "testGitHubLink"
+       }
+      }
+    }
+  }
+}
+  const wrapper = mount(<App />);
+  wrapper.setState({ data: mock, currentMentor: 'testMentor'});
+  const elems = [
+    '.app',
+    '.appHeader',
+    '.appBody',
+    '.tasksList',
+    '.studentsList'
+  ]
+  elems.forEach(item => {
+    expect(wrapper.find(item).exists()).toBe(true);
+  })
 });
